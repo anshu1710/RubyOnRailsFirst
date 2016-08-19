69329a217fab2ad6a9e5da913b8f4b9a929b9a15
anshu1710 <anshu.maheshwari@gmail.com>
2016-08-18 21:52:28 -0400

    Small Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'admin#index'
   get 'admin/index'
 
   #get 'sessions/index'
@@ -18,7 +19,5 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
-  root 'admin#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
